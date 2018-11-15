52598c2b7d70b3f849a43c859e8c9f0a915ba27f
Precy <1127849901@qq.com>
2018-11-16 01:21:08 +0800

    first

---
+++ src/router/index.js
@@ -0,0 +1,39 @@
+/*
+路由器对象模块
+ */
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import MSite from '../pages/MSite/MSite'
+import Order from '../pages/Order/Order'
+import Profile from '../pages/Profile/Profile'
+import Search from '../pages/Search/Search'
+
+//声明使用插件
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  //所有路由
+  routes: [
+    {
+      path: '/msite',
+      component: MSite
+    },
+    {
+      path: '/order',
+      component: Order
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/',
+      redirect: '/msite'
+    }
+  ]
+})
